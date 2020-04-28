2574a712d1a1232296ab72991dc16acf06925dac
faaizz <fr33ziey@gmail.com>
2020-04-28 18:46:47 +0200

    Deployed version 1.1-SNAPSHOT

---
+++ deploy/v1.1-SNAPSHOT/launcher.bat
@@ -0,0 +1,35 @@
+::SETUP SAVE FILE
+
+@ECHO OFF
+
+::Check if save folder exists
+IF EXIST "settings" (
+    ::IF IT DOES, NAVIGATE IN AND CHECK IF save file DOESN'T EXIST
+    CD settings
+    IF NOT EXIST "main.json" (
+        ::CREATE main.json FILE
+        copy NUL main.json
+
+    )
+
+) ELSE (
+    ::IF IT DOESN'T
+    ::CREATE THE settings DIRECTORY  
+    MKDIR settings
+    ::ENTER THE DIRECTORY
+    CD settings
+    ::CREATE main.json FILE
+    copy NUL main.json
+
+)
+
+::NAVIAGATE OUT OF settings FOLDER
+CD ../
+
+::============================================================
+
+::SET TO USE LOCAL JAVA FX SDK
+SET module_path="windows\javafx-sdk-11.0.2\lib" 
+
+::LAUNCH APPLICATION
+java -jar --module-path %module_path% --add-modules=javafx.controls,javafx.fxml java_admin_frontend-1.0-SNAPSHOT.jar
